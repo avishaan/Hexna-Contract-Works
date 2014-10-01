83351bd592b8884596fd33945ed704308d4c5f40
Avnish Mangal <avnish@outlook.com>
2014-10-01 15:28:01 -0400

    Added button with event listener.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
 	//Header
-	var headerTitle = $("<h1>Hexna Contract Works</h1>");
-	var headerDesc = $("<p>Let Hexna give you peace of mind when you choose to sublease your property. Preparing a sublease is time-consuming and often difficult. If you don't use the proper legal language in drafting your arrangement, you're left with no recourse if something goes wrong. Using our software, you can easily create legally binding, airtight leases in a matter of minutes.</p>");
-	$("header").append(headerTitle);
-	$("header").append(headerDesc);
+	var header = $("<h1>Hexna Contract Works</h1>");
+	$("header").append(header);
 
 	//Footer
 	var footer = $("<br><p>Hexna Corp.</p><p>http://www.hexna.com</p>")
 	$("footer").append(footer);
 
-	//Cost-Basis Wizard
-	var costBasisTitle = $("<h3>Cost-Basis Wizard</h3>");
-	$("#costBasis").prepend(costBasisTitle);
+	//Initial
+	var initialDesc = $("<div id=\"initialDesc\"><p>Let Hexna give you peace of mind when you choose to sublease your property. Preparing a sublease is time-consuming and often difficult. If you don't use the proper legal language in drafting your arrangement, you're left with no recourse if something goes wrong. Using our software, you can easily create legally binding, airtight leases in a matter of minutes.</p></div>");
+	$("section").append(initialDesc);
+	var nextButton = $("<button type=button>Let's begin</button>");
+	$("section").append(nextButton);
+
+	$(nextButton).on("click",function() {
+		$("section #initialDesc").remove();
+		$("button").remove();
+		//Cost-Basis Wizard
+		var costBasisTitle = $("<h3>Cost-Basis Wizard</h3>");
+		$("section").prepend(costBasisTitle);
+	});
 });
