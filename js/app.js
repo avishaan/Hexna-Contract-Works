cb9f9fe1664c629d71a06384ee538fc018486c71
Avnish Mangal <avnish@outlook.com>
2014-10-02 10:36:06 -0400

    What happens if you click the footer?

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
 	//Footer
 	var footer = $("<br><p>Hexna Corp.</p><p>http://www.hexna.com</p>")
 	$("footer").append(footer);
+	$("footer").on("click", function(){
+		alert("Why would you click that?\n\nInteresting...");
+	})
 
 	//Initial
 	var initialDesc = $("<div id=\"initialDesc\"><p>Let Hexna give you peace of mind when you choose to sublease your property. Preparing a sublease is time-consuming and often difficult. If you don't use the proper legal language in drafting your arrangement, you're left with no recourse if something goes wrong. Using our software, you can easily create legally binding, airtight leases in a matter of minutes.</p></div>");
